7e065195ce73bb33005b465df8ff0f2bb00e513a
2prime <luyiping9712@pku.edu.cn>
2024-06-24 21:57:37 -0400

    Update IDS2SVD.m

--- IDS2SVD.m
+++ IDS2SVD.m
@@ -1,40 +1,45 @@
-
 function [x_list,time_list] = IDS2SVD(A,b,S,lambda,n,J,t)
+
+scale = vecnorm(A);
+A= A ./ scale;
 
 SA = S*A;
 SSb = transpose(S)*(S*b);
 Ab = transpose(A)*SSb;
 
 
+
+
 [U,svals,V] = svd(SA,'econ'); svals = diag(svals);
+svals2 = svals.*svals;
 
 tic
-x = V*(((V'*Ab)./svals)./svals);
+x = V*(((V'*Ab)./svals2));
 
 time = toc;
 bonus = x;
-x_list = [x];
+x_list = [x ./ scale.'];
 time_list = [time];
 for i = 1:t-1
     tic
     for j = 1:J
     r1 = (transpose(A)*((b-A*x))-lambda*(x));
-    uj = IDS2in(A,r1,V,svals,lambda,J,i+1);
+    uj = IDS2in(A,r1,V,svals2,lambda,J,i+1);
     x = x+uj;
     end
-    x_list = [x_list,x];
+    x_list = [x_list,x ./ scale.'];
     time = toc;
     time_list = [time_list,time];
 end
 end
 
 
-function x = IDS2in(A,Ab,V,svals,lambda,J,t)
+function x = IDS2in(A,Ab,V,svals2,lambda,J,t)
 
 
-xy = V*(((V'*Ab)./svals)./svals);;
+xy = V*(((V'*Ab)./svals2));;
 r2 = Ab-(transpose(A)*(A*xy));
-xy1 = V*(((V'*r2)./svals)./svals);;
+xy1 = V*(((V'*r2)./svals2));;
 X = [xy,xy1];
 ARY=A*X;
 
@@ -46,11 +51,9 @@ x = X*a;
 if t>1
     for i = 1:J
     r1 = Ab-(transpose(A)*(A*x)-lambda*(x));
-    u =  IDS2in(A,r1,V,svals,lambda,J,t-1);
+    u =  IDS2in(A,r1,V,svals2,lambda,J,t-1);
     x=x+u;
     end
 end
 
 end
-
-
